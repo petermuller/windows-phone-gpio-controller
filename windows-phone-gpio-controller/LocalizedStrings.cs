ebdda66acb9681ec90aa923aeb9523c1c3806382
Peter Muller <pmm5983@rit.edu>
2013-07-06 21:32:38 -0400

    Initial commit of Visual Studio files.

---
+++ windows-phone-gpio-controller/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using windows_phone_gpio_controller.Resources;
+
+namespace windows_phone_gpio_controller
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
