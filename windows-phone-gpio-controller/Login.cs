6f02e1ae4540fb50842eb775adff1b70285e2ea8
Peter Muller <pmm5983@rit.edu>
2013-07-07 22:30:57 -0400

    Login.cs: added small skeleton for login page.

---
+++ windows-phone-gpio-controller/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windows_phone_gpio_controller
+{
+    class Login
+    {
+        //Class variables
+        String hostname;
+        String username;
+        String password;
+
+        //Class methods
+        public void getConnection()
+        {
+            //Insert a function for connecting to the raspberry pi.
+        }
+    }
+}
